70521043c8ab17c916c6df234c719baa847e6c23
Flokrates2301 <67049493+Flokrates2301@users.noreply.github.com>
2022-03-14 21:06:08 +0100

    code splitting

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,16 @@ import { store } from './store.js'
 import { Provider } from 'react-redux'
 import { useMediaQuery } from 'react-responsive'
 import { Container } from 'react-bootstrap'
-import MainDesktop from './modules/main/MainDesktop/MainDesktop'
+//import MainDesktop from './modules/main/MainDesktop/MainDesktop'
 import MainTabletLeft from './modules/main/MainTablet/MainTabletLeft'
 import MainTabletRight from './modules/main/MainTablet/MainTabletRight'
 import MainMobile from './modules/main/MainMobile/MainMobile'
 import './App.css'
 
-/*
 const MainDesktop = React.lazy(() =>
   import('./modules/main/MainDesktop/MainDesktop')
 )
+/*
 const MainTabletLeft = React.lazy(() =>
   import('./modules/main/MainTablet/MainTabletLeft')
 )
