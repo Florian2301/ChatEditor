b31546c340615dbed3b3f45aa14d88858044ddb5
Flokrates2301 <67049493+Flokrates2301@users.noreply.github.com>
2022-03-14 18:09:35 +0100

    webpack config optimize build

--- .webpack.config.js
+++ .webpack.config.js
@@ -7,7 +7,25 @@ module.exports = {
       zlib: require.resolve('browserify-zlib'),
     },
   },
-  plugins: [new NodePolyfillPlugin()],
+  plugins: [
+    new NodePolyfillPlugin(),
+    new webpack.DefinePlugin({
+      //<--key to reduce React's size
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
   mode: 'development',
   target: 'node',
   module: {
