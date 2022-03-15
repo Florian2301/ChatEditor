782c6a52b4e060e85a6e1877175e248f256f0c65
Flokrates2301 <67049493+Flokrates2301@users.noreply.github.com>
2022-03-15 22:21:45 +0100

    comment out keys

--- server/server.js
+++ server/server.js
@@ -1,26 +1,26 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+// Use Routes
+import chatRouter from './routes/api/userChats/userChats.js';
+import draftRouter from './routes/api/UserDrafts/userDrafts.js';
+import mongoose from 'mongoose';
 import path from 'path';
-import mongoURI from './config/keys.js';
+import titleRouter from './routes/api/userTitle/userTitle.js';
+import userRouter from './routes/api/users/users.js';
+//import mongoURI from './config/keys.js'
 const app = express();
 // BodyParser Middleware
 app.use(bodyParser.json());
 // DB Config
-const db = process.env.mongoURI || mongoURI;
+const db = process.env.mongoURI; //|| mongoURI
 // Connect to MongoDB
 mongoose
     .connect(db)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.log(err));
-// Use Routes
-import chatRouter from './routes/api/userChats/userChats.js';
 app.use('/api/chats', chatRouter);
-import titleRouter from './routes/api/userTitle/userTitle.js';
 app.use('/api/title', titleRouter);
-import draftRouter from './routes/api/UserDrafts/userDrafts.js';
 app.use('/api/drafts', draftRouter);
-import userRouter from './routes/api/users/users.js';
 app.use('/api/users', userRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from 'express'
-import mongoose from 'mongoose'
+
 import bodyParser from 'body-parser'
+// Use Routes
+import chatRouter from './routes/api/userChats/userChats.js'
+import draftRouter from './routes/api/UserDrafts/userDrafts.js'
+import mongoose from 'mongoose'
 import path from 'path'
-import mongoURI from './config/keys.js'
+import titleRouter from './routes/api/userTitle/userTitle.js'
+import userRouter from './routes/api/users/users.js'
+//import mongoURI from './config/keys.js'
 
 const app = express()
 
@@ -10,7 +16,7 @@ const app = express()
 app.use(bodyParser.json())
 
 // DB Config
-const db = process.env.mongoURI || mongoURI
+const db = process.env.mongoURI //|| mongoURI
 
 // Connect to MongoDB
 mongoose
@@ -18,17 +24,12 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch((err: string) => console.log(err))
 
-// Use Routes
-import chatRouter from './routes/api/userChats/userChats.js'
 app.use('/api/chats', chatRouter)
 
-import titleRouter from './routes/api/userTitle/userTitle.js'
 app.use('/api/title', titleRouter)
 
-import draftRouter from './routes/api/UserDrafts/userDrafts.js'
 app.use('/api/drafts', draftRouter)
 
-import userRouter from './routes/api/users/users.js'
 app.use('/api/users', userRouter)
 
 // Serve static assets if in production
