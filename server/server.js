7f099f6ea2f573333ca95a8b987dedffb589bf23
Flokrates2301 <67049493+Flokrates2301@users.noreply.github.com>
2022-03-28 17:28:53 +0200

    adjustment for deployment

--- server/server.js
+++ server/server.js
@@ -2,17 +2,22 @@ import bodyParser from 'body-parser'
 import chatRouter from './dist/routes/api/userChats/userChats.js'
 import draftRouter from './dist/routes/api/UserDrafts/userDrafts.js'
 import express from 'express'
-import mongoURI from './dist/config/keys.js' // comment out for deployment on heroku
 import mongoose from 'mongoose'
 import path from 'path'
 import titleRouter from './dist/routes/api/userTitle/userTitle.js'
 import userRouter from './dist/routes/api/users/users.js'
 
+//import mongoURI from './dist/config/keys.js' // comment out for deployment on heroku
+
+
+
+
+
 const app = express()
 // BodyParser Middleware
 app.use(bodyParser.json())
 // DB Config
-const db = process.env.mongoURI || mongoURI
+const db = process.env.mongoURI //|| mongoURI
 // Connect to MongoDB
 mongoose
   .connect(db)
